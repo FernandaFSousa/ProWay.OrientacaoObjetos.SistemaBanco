a93c10edd32e15d45c5f4bbecbf0afbcbe14766c
José Klaumann <jose.augusto.klaumann@hotmail.com>
2021-12-13 20:53:45 -0300

    Adicionado estrutura de banco de dados com o Banco -Criado arquivo de estrutura de tabelas -Criado o banco de dados -Criado o projet de windws forms -Criado o projeto de serviços

---
+++ ProWay.OrientacaoObjetos.Views/BancoDados/Estrutura.sql
@@ -0,0 +1,19 @@
+﻿DROP TABLE ContasPoupanca;
+
+CREATE TABLE ContasPoupanca(
+codigo INT PRIMARY KEY IDENTITY,
+nome VARCHAR(100) NOT NULL,
+saldo DECIMAL(6, 2)NOT NULL
+);
+
+INSERT INTO ContasPoupanca (nome, saldo)
+VALUES ('Fernanda', 11.49)
+
+SELECT *FROM ContasPoupanca;
+
+--VARCHAR(100) campo de texto permite até 100 caracter 
+--DECIMAL numerp do tipo real
+--INT inteiro 
+--IDENTITY para ser auto incremento a coluna, ou seja, primeiro registro 1,2,3
+--PRIMARY KEY chave primaria
+--NOT NULL não permite nulo
---
+++ ProWay.OrientcaoObjeto.Services/Contas/ContaPoupancaService.cs
@@ -0,0 +1,29 @@
+﻿using ProWay.OrientacaoObjetos.Models.ContaBancaria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProWay.OrientcaoObjeto.Services.Contas
+{
+    internal class ContaPoupancaService
+    {
+        /*
+         * CRUD
+         * create: criar a conta poupança 
+         * read: ler a conta poupanca 
+         * Update: atualizar a conta poupança 
+         * delete: apagar a conta poupança
+         */
+
+        public void Adicionar(ContaPoupanca contaPoupanca)
+        {
+
+        }
+         //Adicionar()
+         //Atualizar()
+         //Apagar()
+         //Listar()
+    }
+}
