11d868cc5da09cdae6baa32b81b32bd5b27e20f9
José Klaumann <jose.augusto.klaumann@hotmail.com>
2021-12-13 22:08:01 -0300

    Adicionar a funcionalidade de salvar conta poupanca e listar

--- ProWay.OrientacaoObjetos.Models/ContaBancaria/ContaPoupanca.cs
+++ ProWay.OrientacaoObjetos.Models/ContaBancaria/ContaPoupanca.cs
@@ -8,6 +8,15 @@ namespace ProWay.OrientacaoObjetos.Models.ContaBancaria
 {
     public class ContaPoupanca : Conta
     {
-       
+        //Construtor é uma forma de garantir que o bjeto
+        //é construido de forma correta, ou seja, neste
+        //cenario para abrir uma conta poupança é obrigatorio
+        //ter nome e saldo da conta antes de conseguir abrir
+        //efetivamente
+       public ContaPoupanca(string nome, double saldo)
+        {
+            Nome = nome;
+            Saldo = saldo;
+        }
     }
 }
---
+++ ProWay.OrientacaoObjetos.Views/Contas/FormContaPoupanca.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ProWay.OrientacaoObjetos.Views.Contas
+{
+    partial class FormContaPoupanca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnSaldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelCodigo = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelSaldo = new System.Windows.Forms.Label();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.textBoxCodigo = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxSaldo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.ColumNome,
+            this.ColumnSaldo});
+            this.dataGridView.Location = new System.Drawing.Point(12, 53);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(474, 356);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Codigo";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // ColumNome
+            // 
+            this.ColumNome.HeaderText = "Nome";
+            this.ColumNome.Name = "ColumNome";
+            this.ColumNome.ReadOnly = true;
+            // 
+            // ColumnSaldo
+            // 
+            this.ColumnSaldo.HeaderText = "Saldo";
+            this.ColumnSaldo.Name = "ColumnSaldo";
+            this.ColumnSaldo.ReadOnly = true;
+            // 
+            // labelCodigo
+            // 
+            this.labelCodigo.AutoSize = true;
+            this.labelCodigo.Location = new System.Drawing.Point(517, 28);
+            this.labelCodigo.Name = "labelCodigo";
+            this.labelCodigo.Size = new System.Drawing.Size(46, 15);
+            this.labelCodigo.TabIndex = 1;
+            this.labelCodigo.Text = "Codigo";
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(517, 84);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 2;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelSaldo
+            // 
+            this.labelSaldo.AutoSize = true;
+            this.labelSaldo.Location = new System.Drawing.Point(517, 141);
+            this.labelSaldo.Name = "labelSaldo";
+            this.labelSaldo.Size = new System.Drawing.Size(36, 15);
+            this.labelSaldo.TabIndex = 3;
+            this.labelSaldo.Text = "Saldo";
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(327, 24);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 4;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(411, 24);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.TabIndex = 5;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(713, 199);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvar.TabIndex = 6;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // textBoxCodigo
+            // 
+            this.textBoxCodigo.Location = new System.Drawing.Point(517, 46);
+            this.textBoxCodigo.Name = "textBoxCodigo";
+            this.textBoxCodigo.Size = new System.Drawing.Size(271, 23);
+            this.textBoxCodigo.TabIndex = 7;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(517, 102);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(271, 23);
+            this.textBoxNome.TabIndex = 8;
+            // 
+            // textBoxSaldo
+            // 
+            this.textBoxSaldo.Location = new System.Drawing.Point(517, 159);
+            this.textBoxSaldo.Name = "textBoxSaldo";
+            this.textBoxSaldo.Size = new System.Drawing.Size(271, 23);
+            this.textBoxSaldo.TabIndex = 9;
+            // 
+            // FormContaPoupanca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxSaldo);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxCodigo);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.labelSaldo);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.labelCodigo);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "FormContaPoupanca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormContaPoupanca";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn ColumNome;
+        private DataGridViewTextBoxColumn ColumnSaldo;
+        private Label labelCodigo;
+        private Label labelNome;
+        private Label labelSaldo;
+        private Button buttonEditar;
+        private Button buttonApagar;
+        private Button buttonSalvar;
+        private TextBox textBoxCodigo;
+        private TextBox textBoxNome;
+        private TextBox textBoxSaldo;
+    }
+}
